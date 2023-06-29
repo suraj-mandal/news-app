084cf14369b98207dc62f09ccfbc4e3f8d67d00f
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-06-29 16:04:04 +0530

    fix

--- app/src/kotlin/button.kt
+++ app/src/kotlin/button.kt
@@ -1,61 +1,18 @@
-import androidx.compose.material.Button
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.navigation.Navigation
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.SignInActivity
+import kotlinx.android.synthetic.main.siginin.*
 
+class YourActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.your_layout_file)
 
-Button(
-onClick = {
-    private fun navigateToSignInPage() {
-    val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-    navController.navigate(R.id.signinFragment)
-}
-
-    val button = findViewById<Button>(R.id.your_button_id)
-    button.setOnClickListener {
-        navigateToSignInPage()
-    }
-
-},
-modifier = Modifier
-.offset(x = 94.dp, y = 596.dp)
-.width(187.dp)
-.height(45.dp)
-.background(color = Color(0xFF4760FF), shape = RoundedCornerShape(size = 36.dp))
-) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Text(
-            text = "Continue",
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-            fontSize = 16.sp
-        )
-        Icon(
-            imageVector = Icons.Default.KeyboardArrowRight,
-            contentDescription = null,
-            tint = Color.White,
-            modifier = Modifier.padding(start = 4.dp)
-        )
+        yourButton.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
-
-
-
-
-
-
-
-
